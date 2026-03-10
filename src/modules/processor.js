c755953a7849d726bbe26adad68d8f9b777a87f3
GosuDRM <gianpacayra@gmail.com>
2026-03-10 19:14:11 +0800

    fix(ad-end): include channel on worker AdEnded events

--- src/modules/processor.js
+++ src/modules/processor.js
@@ -197,7 +197,7 @@ async function _processM3U8(url, text, realFetch) {
 			__TTVAB_STATE__.PinnedBackupPlayerChannel = null;
 			_log("Ad ended", "success");
 			if (typeof self !== "undefined" && self.postMessage) {
-				self.postMessage({ key: "AdEnded" });
+				self.postMessage({ key: "AdEnded", channel: info.ChannelName });
 				if (wasUsingModifiedM3U8 || __TTVAB_STATE__.ReloadPlayerAfterAd) {
 					self.postMessage({
 						key: "ReloadPlayer",
