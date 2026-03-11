48de40184caac0126b855ebece4b5bded9d3a456
GosuDRM <gianpacayra@gmail.com>
2026-03-11 13:37:36 +0800

    Make stream info ad comparator boolean-safe

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -613,7 +613,7 @@ function _$gsi(url) {
 		const bTime = b?.LastActivityAt || 0;
 		if (bTime !== aTime) return bTime - aTime;
 		if (a?.IsShowingAd !== b?.IsShowingAd) {
-			return Number(b?.IsShowingAd) - Number(a?.IsShowingAd);
+			return (b?.IsShowingAd ? 1 : 0) - (a?.IsShowingAd ? 1 : 0);
 		}
 		return 0;
 	})[0];
