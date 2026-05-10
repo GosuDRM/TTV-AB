6d511c98284227d45a08035936cbf28cdbdaade2
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-05-10 08:14:10 +0800

    release: v7.0.4

--- tests/constants.test.ts
+++ tests/constants.test.ts
@@ -19,7 +19,7 @@ describe("_C constants", () => {
 	const C = () => g._C as Record<string, unknown>;
 
 	it("has valid version", () => {
-		expect(C().VERSION).toBe("7.0.3");
+		expect(C().VERSION).toBe("7.0.4");
 	});
 
 	it("has positive INTERNAL_VERSION", () => {
