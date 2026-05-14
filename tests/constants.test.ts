a844d994eaef8932675e0e0ea06d16bedb2e399c
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-05-14 12:08:49 +0800

    test: bump version assertion to 7.3.1

--- tests/constants.test.ts
+++ tests/constants.test.ts
@@ -20,7 +20,7 @@ describe("_C constants", () => {
 	const C = () => g._C as Record<string, unknown>;
 
 	it("has valid version", () => {
-		expect(C().VERSION).toBe("7.2.1");
+		expect(C().VERSION).toBe("7.3.1");
 	});
 
 	it("has positive INTERNAL_VERSION", () => {
