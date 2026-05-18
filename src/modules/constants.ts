06f0025133036b4326050a213d04d969640cca8b
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-05-19 06:23:38 +0800

    chore: bump to v8.5.5

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,8 +1,8 @@
 // TTV AB - Constants
 
 const _C = {
-	VERSION: "8.5.4",
-	INTERNAL_VERSION: 172,
+	VERSION: "8.5.5",
+	INTERNAL_VERSION: 173,
 	LOG_STYLES: {
 		prefix:
 			"background: linear-gradient(135deg, #9146FF, #772CE8); color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;",
