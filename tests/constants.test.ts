22c3e589776a190fd8c315d2b086d70306547708
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-05-19 06:23:36 +0800

    chore: bump to v8.5.5

--- tests/constants.test.ts
+++ tests/constants.test.ts
@@ -20,7 +20,7 @@ describe("_C constants", () => {
 	const C = () => g._C as Record<string, unknown>;
 
 	it("has valid version", () => {
-		expect(C().VERSION).toBe("8.5.4");
+		expect(C().VERSION).toBe("8.5.5");
 	});
 
 	it("has positive INTERNAL_VERSION", () => {
