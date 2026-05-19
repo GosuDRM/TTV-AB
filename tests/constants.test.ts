f411a2ab5e4c9a6839ad6f253cce984bd53c1903
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-05-20 05:06:30 +0800

    chore(release): bump extension package version to 8.8.3

--- tests/constants.test.ts
+++ tests/constants.test.ts
@@ -20,7 +20,7 @@ describe("_C constants", () => {
 	const C = () => g._C as Record<string, unknown>;
 
 	it("has valid version", () => {
-		expect(C().VERSION).toBe("8.8.2");
+		expect(C().VERSION).toBe("8.8.3");
 	});
 
 	it("has positive INTERNAL_VERSION", () => {
