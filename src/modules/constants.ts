da784a2e1b31ea962cca78aaf4db2119afbd0de4
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-06-07 14:56:50 +0800

    chore(release): bump version to 9.3.3

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,8 +1,8 @@
 // TTV AB - Constants
 
 const _C = {
-	VERSION: "9.3.2",
-	INTERNAL_VERSION: 210,
+	VERSION: "9.3.3",
+	INTERNAL_VERSION: 211,
 	LOG_STYLES: {
 		prefix:
 			"background: linear-gradient(135deg, #9146FF, #772CE8); color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;",
