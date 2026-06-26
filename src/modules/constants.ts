28a5ebbe8d723ea2ffdf65a0bf7c58051660902a
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-06-27 00:06:34 +0800

    release: v10.0.2 - fix silent hold recovery
    Refs #36

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,8 +1,8 @@
 // TTV AB - Constants
 
 const _C = {
-	VERSION: "10.0.1",
-	INTERNAL_VERSION: 100001,
+	VERSION: "10.0.2",
+	INTERNAL_VERSION: 100002,
 	LOG_STYLES: {
 		prefix:
 			"background: linear-gradient(135deg, #9146FF, #772CE8); color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;",
