ac2d41c7ab54cffa204073ce67a693f414126767
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-07-25 18:55:32 +0800

    release: v13.2.1

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,8 +1,8 @@
 // TTV AB - Constants
 
 const _C = {
-	VERSION: "13.2.0",
-	INTERNAL_VERSION: 130200,
+	VERSION: "13.2.1",
+	INTERNAL_VERSION: 130201,
 	LOG_STYLES: {
 		prefix:
 			"background: linear-gradient(135deg, #9146FF, #772CE8); color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;",
