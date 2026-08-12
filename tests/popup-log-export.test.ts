4868da8eb12805bdaf7812325e02167ae6b5edd0
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-08-12 21:34:14 +0800

    release: bump version to 16.0.5

--- tests/popup-log-export.test.ts
+++ tests/popup-log-export.test.ts
@@ -33,7 +33,7 @@ beforeAll(() => {
 	g.chrome = {
 		runtime: {
 			lastError: null,
-			getManifest: () => ({ version: "16.0.4" }),
+			getManifest: () => ({ version: "16.0.5" }),
 		},
 		tabs: {
 			query: () => {},
