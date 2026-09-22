f8217b6382926508c240c926cb1b620d8a0dbf59
Gian Carlo Pacayra <gianpacayra@gmail.com>
2026-09-23 05:19:55 +0800

    release: bump version to 18.0.6

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,8 +1,8 @@
 // TTV AB - Constants
 
 const _C = {
-	VERSION: "18.0.5",
-	INTERNAL_VERSION: 180005,
+	VERSION: "18.0.6",
+	INTERNAL_VERSION: 180006,
 	LOG_STYLES: {
 		prefix:
 			"background: linear-gradient(135deg, #9146FF, #772CE8); color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;",
